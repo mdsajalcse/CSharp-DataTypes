68331dcb1982dbc19354e018fca54c6cd4284430
sajal <sajalcse2018@gmail.com>
2023-09-23 14:57:52 +0600

    Csharp data type

---
+++ CSharp-DataTypes/Program.cs
@@ -0,0 +1,90 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("C# Data Types");
+
+//integer Data
+int a = 5;
+Console.WriteLine("It is integer:" + a);
+Console.WriteLine("Integer Data Size=" + sizeof(int));
+
+//unsigned Data
+uint b = 10;
+Console.WriteLine("It is Unsigned Integer:" + b);
+Console.WriteLine("Unsigned Integer Data Size:" + sizeof(uint));
+
+//short Data Type
+
+short shortData = 4;
+Console.WriteLine("It is short Data:" + shortData);
+Console.WriteLine("Short Data Size:" + sizeof(short));
+
+//Ushort Data type
+
+ushort uShortData = 4;
+Console.WriteLine("It is unsigned short Data:" + uShortData);
+Console.WriteLine("UShort Data Size:" + sizeof(ushort));
+
+//Byte Data
+byte byData = 55;
+Console.WriteLine("It is Byte Data:" + byData);
+Console.WriteLine("Byte Data Size:" + sizeof(byte));
+
+//sbyte Data
+sbyte SByte = 20;
+Console.WriteLine("It is Sbyte Data:" + SByte);
+Console.WriteLine("Sbyte Data Size:" + sizeof(sbyte));
+
+//float data type
+float floatData = 50.9f;
+Console.WriteLine("It is float Data:" + floatData);
+Console.WriteLine("float Data Size" + sizeof(float));
+
+//double data type
+double doubleData = 5.254587545;
+Console.WriteLine("It is Double Data:" + doubleData);
+Console.WriteLine("Double Data Size:" + sizeof(double));
+
+//Decimal Data Type
+decimal decimalData = 10.5m;
+Console.WriteLine("It is Decimal Data:" + decimalData);
+Console.WriteLine("Decimal Data Size:" + sizeof(decimal));
+
+//Long Data type
+long longData = 258749865;
+Console.WriteLine("It is Long Data:" + longData);
+Console.WriteLine("Long Data Size:" + sizeof(long));
+
+//ulong data type
+ulong uLongData = 555555474845845;
+Console.WriteLine("It is ULong Data:" + uLongData);
+Console.WriteLine("ULong Data Size:" + sizeof(ulong));
+
+
+//boolean data type
+
+bool isDoCoding = true;
+Console.WriteLine("It is Boolean data:" + isDoCoding);
+Console.WriteLine("Boolean data Size:" + sizeof(bool));
+
+//string Data
+
+string stringData = "Hello C Sharp Programming";
+Console.WriteLine("It is String Data Type:" + stringData);
+
+//char data type
+
+char ch = s;
+Console.WriteLine("It is Character:" + ch);
+Console.WriteLine("Char Size is:" + sizeof(char));
+
+//Date Time Data
+
+DateTime dateTime = DateTime.Now;
+Console.WriteLine("It is DateTime Data:" + dateTime);
+//Console.WriteLine("Date Time Size:" + sizeof(DateTime));
+
+
+
+
+
+
+
